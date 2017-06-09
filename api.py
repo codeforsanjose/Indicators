bd6b0e4aad8a5b6a944b3089c6787932fbd7e4f2
Joseph <jrichardson@jmstudios.net>
2017-06-08 19:45:03 -0700

    connected to api

--- api.py
+++ api.py
@@ -1,5 +1,24 @@
+import requests
+from requests.auth import HTTPBasicAuth
+
+apikey = "d8fa9f7c0841efecfb91b98bf8cbe056cf654cec"
+request_url = "http://citysdk.commerce.gov"
+
+
 def main():
-    print("hello")
+    request_obj = {
+        'zip': '21401',
+        'state': 'MD',
+        'level': 'state',
+        'sublevel': False,
+        'api': 'acs5',
+        'year': 2010,
+        'variables': ['income', 'population']
+    }
+    response = requests.post(request_url, auth=HTTPBasicAuth(apikey, None), json=request_obj)
+
+    print(response.json())
+
 
 if __name__ == "__main__":
     main()
