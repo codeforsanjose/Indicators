8ef2f2eae1111c7d61cbddad42e0b98ab45be867
manika15 <manika.kapoor90@gmail.com>
2017-08-10 20:36:13 -0700

    add class to handle trace

---
+++ trace_generator.py
@@ -0,0 +1,21 @@
+import plotly
+import plotly.plotly as py
+import plotly.graph_objs as go
+import numpy as np
+
+plotly.tools.set_credentials_file(username='manika15', api_key='dogNq5ILvZnKd5BzKOry')
+
+class TraceGenerator():
+    def __init__(self):
+        pass
+
+    def get_trace(self, values, years, name, color):
+        trace = go.Scatter(
+            x=years,
+            y=values,
+            name=name,
+            line=dict(
+                color=(color),
+                width=4)
+        )
+        return trace
