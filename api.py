79cdcdefda6bbdfe8a73f7a3ae1764a892ae7478
Joseph <jrichardson@jmstudios.net>
2017-08-14 17:23:26 -0700

    removed unused stuff in api.py

--- api.py
+++ api.py
@@ -14,53 +14,6 @@ apikey = "d8fa9f7c0841efecfb91b98bf8cbe056cf654cec"
 #url to access
 request_url = "http://citysdk.commerce.gov"
 
-def cali_income(year):
-    c = Census("d8fa9f7c0841efecfb91b98bf8cbe056cf654cec")
-
-    # household with income >150K and <200k in California
-    cali1 = c.acs5.state('B19001_016E', states.CA.fips, year=year)
-    # household with income >200k in California
-    cali2 = c.acs5.state('B19001_017E', states.CA.fips, year=year)
-    # households with income California
-    cali_pop = c.acs5.state('B19051_002E', states.CA.fips, year=year)
-    # share of households with income >150K in California
-    cali_tot = sum(int(item['B19001_016E']) for item in cali1) + \
-               sum(int(item['B19001_017E']) for item in cali2)
-    cali_tot_pop = sum(int(item['B19051_002E']) for item in cali_pop)
-    cali_ratio = ((cali_tot) / cali_tot_pop * 100)
-    cali_ratio = round(cali_ratio, 1)
-
-    return cali_ratio
-
-def us_income(year):
-    c = Census("d8fa9f7c0841efecfb91b98bf8cbe056cf654cec")
-
-    # household with income >150K and <200k in US
-    us_tot1 = c.acs5.state('B19001_016E', Census.ALL, year=year)
-    # household with income >200k in US
-    us_tot2 = c.acs5.state('B19001_017E', Census.ALL, year=year)
-    #households with income US
-    us_pop = c.acs5.state('B19051_002E', Census.ALL, year=year)
-    #share of households with income >150K in US
-    us_tot= sum(int(item['B19001_016E']) for item in us_tot1) + \
-            sum(int(item['B19001_017E']) for item in us_tot2)
-    us_tot_pop = sum(int(item['B19051_002E']) for item in us_pop)
-
-    return us_tot_pop
-
-def income_150k_plot():
-    income_cali = [cali_income(year) for year in years]
-    # households with income >150k in United States
-    income_us = [us_income(year) for year in years]
-
-    population_cali = [cali_population(year) for year in years]
-    # households with income in United States
-    population_us = [us_population(year) for year in years]
-
-    # share of households with income greater than 150k in California state
-    ratio_cali = share(income_cali, population_cali)
-    # share of households with income greater than 150k in United states
-    ratio_us = share(income_us, population_us)
 
 def main():
     county_ids = dict(
